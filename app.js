9e5acaf26cb70f6056204ef4a3906261b023fefe
ckroening <charlottekroening@gmail.com>
2016-01-03 11:30:59 -0600

    completed assignment.

--- app.js
+++ app.js
@@ -1,44 +1,37 @@
 /**
- * Queue implemenation
- * @author: TODO
+ * Queue Implemenation: QUnit Testing
+ * @author: Charlotte Kroening
  */
 
  function Queue() {
  	var elements = [];
 
- 	/**
- 	 * Adds an element to back of queue
- 	 *
- 	 * @param {*} element to add
- 	 */
+ 	/* Adds an element to back of queue, @param {*} element to add */
  	this.enqueue = function(element) {
- 		return null; // TODO
- 	}
- 	//TODO: add doc block
+ 		elements.push(element);
+ 	};
+
+ 	/* Remove and return first element from front of queue. */
  	this.dequeue = function() {
- 		return null; // TODO
- 	}
- 	/**
- 	 * Reveals first element in queue.
- 	 * Does NOT remove element.
- 	 *
- 	 * @return first element in queue
- 	 */
+ 		elements.shift();
+ 	};
+
+ 	/* Reveals first element in queue. Does NOT remove element. */
  	this.front = function() {
  		return elements[0];
  	}
- 	/**
- 	 * Determine if queue is empty.
- 	 *
- 	 * @return true if queue has no elements, 
- 	 * 			false if queue has elements
- 	 */
- 	this.isEmpty = function() {
- 		return null; // TODO
- 	}
 
- 	//TODO: add doc block
+ 	/* Determine if queue is empty. @return true if queue has no elements, false if queue has elements */
+ 	this.isEmpty = function() {
+ 		if (elements.length === 0) {
+ 			return true;
+ 		} else {
+ 			return false;
+ 		}
+ 	};
+
+ 	/* Return length of array (number of elements in queue.) */
  	this.size = function() {
- 		return null; // TODO
- 	}
- }
+ 		return elements.length;
+ 	};
+ };
--- tests.js
+++ tests.js
@@ -1,44 +1,68 @@
-QUnit.module("enqueue");
-// TODO: Add tests
+/* QUnit Testing Practice.
+/* Functions to Test: 'enqueue', 'dequeue', 'front', 'isEmpty', 'size' */
 
-QUnit.module("dequeue", {
+/* Test 'enqueue' function: 1. Make sure first element is added to end of queue. */
+QUnit.module('enqueue');
+Qunit.test('Add element to the end of the queue.', function(assert) {
+	this.queue = new Queue();
+	assert.equal(this.queue.enqueue('first'), undefined, 'Tests for addition of first element.');
+	assert.equal(this.queue.enqueue('second'), undefined, 'Tests for addition of second element.')
+});
+
+/* Test 'dequeue' function: 1. Check removal and return of first element in queue. */
+QUnit.module('dequeue', {
 	beforeEach: function() {
 		this.queue = new Queue();
-		this.queue.enqueue("first");
-		this.queue.enqueue("second");
-		this.queue.enqueue("third");
+		this.queue.enqueue('first');
+		this.queue.enqueue('second');
+		this.queue.enqueue('third');
 	}
 });
-QUnit.test("Removes and returns first element in the queued", function(assert) {
-	assert.equal(this.queue.dequeue(), "first", "the first element is returned");
-	assert.equal(this.queue.size(), 2, "the queue is smaller");
 
-	assert.equal(this.queue.dequeue(), "second", "the second element is returned");
-	assert.equal(this.queue.size(), 1, "the queue is smaller");
+QUnit.test('Removes and returns first element in the queued', function(assert) {
+	assert.equal(this.queue.dequeue(), 'first', 'the first element is returned');
+	assert.equal(this.queue.size(), 2, 'the queue is smaller');
 
-	assert.equal(this.queue.dequeue(), "third", "the third element is returned");
-	assert.equal(this.queue.size(), 0, "the queue is smaller");
+	assert.equal(this.queue.dequeue(), 'second', 'the second element is returned');
+	assert.equal(this.queue.size(), 1, 'the queue is smaller');
+
+	assert.equal(this.queue.dequeue(), 'third', 'the third element is returned');
+	assert.equal(this.queue.size(), 0, 'the queue is smaller');
 });
 
-QUnit.module("front");
-// TODO: Add tests
+/* Test 'front' function: 1. Check return of first element without removal. */
+QUnit.module('front');
+Qunit.test('Return first element without removing it.', function (assert) {
+	this.queue = new Queue;
+	assert.equal(this.queue.front(), 'first', 'first element returned.');
+	assert.equal(this.queue.size(), 3, 'queue size has not changed.');
+};
 
-QUnit.module("isEmpty");
-// TODO: Add tests
+/* Test 'isEmpty' function: 1. Boolean check whether or not queue is empty. */
+QUnit.module('isEmpty');
+QUnit.test('Check for an empty queue.', function (assert) {
+	this.queue = new Queue;
+	assert.equal(this.queue.isEmpty(), true, 'queue contains no elements.');
 
-QUnit.module("size", {
+	this.enqueue('first');
+	assert.equal(this.queue.isEmpty(), false, 'queue contains elements.');
+};
+
+/* Test 'size' function: 1. match elements.length to number of elements in queue) */
+QUnit.module('size', {
 	beforeEach: function() {
 		this.queue = new Queue();
 	}
 });
-QUnit.test("Returns correct size", function(assert) {
-	this.queue.enqueue("first");
-	assert.equal(this.queue.size(), 1, "queue has one element");
 
-	this.queue.enqueue("second");
-	assert.equal(this.queue.size(), 2, "queue has two elements");
+QUnit.test('Returns correct size', function(assert) {
+	this.queue.enqueue('first');
+	assert.equal(this.queue.size(), 1, 'queue has one element');
 
-	this.queue.enqueue("third");
-	assert.equal(this.queue.size(), 3, "queue has three elements");
+	this.queue.enqueue('second');
+	assert.equal(this.queue.size(), 2, 'queue has two elements');
+
+	this.queue.enqueue('third');
+	assert.equal(this.queue.size(), 3, 'queue has three elements');
 });
 
